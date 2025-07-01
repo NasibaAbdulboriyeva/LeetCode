3c52604073195179b48949e4f8f164b425301511
Nasiba <nasibaabdulboriyeva71@gmail.com>
2025-07-01 16:09:13 +0500

    solved 1 easy problem in 15 minutes

--- Easy/leetcodeEasy/leetcodeEasy/Program.cs
+++ Easy/leetcodeEasy/leetcodeEasy/Program.cs
@@ -7,4 +7,24 @@
             Console.WriteLine("Hello, World!");
         }
     }
+    public class Solution
+    {
+        public int LengthOfLastWord(string s)///hello world
+        {
+            var count = 0;
+            for (var i = s.Length - 1; i >= 0; i--)
+            {
+                if (s[i] != ' ')
+                {
+                    count++;
+                    
+                }
+               else if (count != 0 && s[i] == ' ')
+                {
+                    return count;
+                }
+            }
+            return count;
+        }
+    }
 }
