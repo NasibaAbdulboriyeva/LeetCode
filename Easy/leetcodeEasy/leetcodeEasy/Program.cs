c572fdef51df119cc66ec562a746d0169fe1d381
Nasiba <nasibaabdulboriyeva71@gmail.com>
2025-07-19 11:21:59 +0500

    added

--- Easy/leetcodeEasy/leetcodeEasy/Program.cs
+++ Easy/leetcodeEasy/leetcodeEasy/Program.cs
@@ -108,4 +108,33 @@ namespace leetcodeEasy
     //}
 
 
+
+    public class Solution
+    {
+        public IList<IList<int>> Combine(int n, int k)
+        {
+            IList<IList<int>> result = new List<IList<int>>();
+            int position = 0;
+            int[] current = new int[k];
+            while (0 <= position)
+            {
+                current[position]++;
+                if (n < current[position])
+                {
+                    position--;
+                }
+                else if (position == k - 1)
+                {
+                    result.Add(current.ToArray());
+                }
+                else
+                {
+                    current[++position] = current[position - 1];
+                }
+            }
+            return result;
+        }
+    }
+
+
 }
